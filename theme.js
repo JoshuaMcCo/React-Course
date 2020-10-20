d1dd83495b345ef92efc51220c41a6aad21e8223
JoshuaMcCo <73140193+JoshuaMcCo@users.noreply.github.com>
2020-10-20 08:20:20 -0600

    Delete theme.js

--- theme.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const { Consumer, Provider } = React.createContext()
-//in order to create a new context we will use react.createContext,
-//that's going to reeturn us an objec that has 2 properties on it.
-
-export const ThemeConsumer = Consumer
-export const ThemeProvider = Provider
